7735b312d1ffd51b0c3c701ee0f5d1379169eccc
hanzs <hanzs@si-tech.com.cn>
2020-05-08 14:46:41 +0800

    docker

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
         proxy: {
             //碰到college路由就会起作用了
             "/api": {
-                target: "http://localhost:9898/",
+                target: "http://47.103.4.57:9898/",
                 changeOrigin: true,
                 //重定向，一般可以不用写，或者值写出空''
                 pathRewrite: {
