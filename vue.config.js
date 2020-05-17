359e7e5e3c2ff6dfe2857e3c109ba7786cd83f6f
hanzs <hanzs@si-tech.com.cn>
2020-05-18 01:17:13 +0800

    线上请求地址

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@ module.exports = {
         proxy: {
             //碰到college路由就会起作用了
             "/api": {
-                // target: "http://47.103.4.57:9898/",
-                target: "http://localhost:9898/",
+                target: "http://47.103.4.57:9898/",
+                // target: "http://localhost:9898/",
                 changeOrigin: true,
                 //重定向，一般可以不用写，或者值写出空''
                 pathRewrite: {
